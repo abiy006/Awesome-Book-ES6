1147b83f76e673ef66106a59282963195b08d313
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-05-04 12:56:58 +0300

    Luxon module is fixed

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { DateTime } from './modules/luxon.js';
+// import { DateTime } from './modules/luxon.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 import BookList from './modules/booklist.js';
 import { handleNavAddClick, handleNavListClick, handleNavContactClick } from './modules/navigation.js';
 
@@ -18,4 +19,4 @@ navAdd.addEventListener('click', handleNavAddClick);
 navList.addEventListener('click', handleNavListClick);
 navContact.addEventListener('click', handleNavContactClick);
 
-// bookList.init();
+bookList.init();
